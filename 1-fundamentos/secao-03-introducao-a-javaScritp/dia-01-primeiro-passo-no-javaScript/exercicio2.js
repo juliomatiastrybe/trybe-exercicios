3e6ca9e579c35ce656bd0bc4acb220350afc4a7c
Julio Matias <juliocesark2@hotmail.com>
2023-07-05 23:26:50 -0300

    adiciona o exercicio2.js e sua resolução

---
+++ 1-fundamentos/secao-03-introducao-a-javaScritp/dia-01-primeiro-passo-no-javaScript/exercicio2.js
@@ -0,0 +1,10 @@
+//Utilize if/else para escrever um código que retorne o maior de dois números.
+//Defina, no começo do seu código duas variáveis com os valores que serão comparados.
+let num1 = 10;
+let num2 = 112;
+
+if (num1 > num2){
+    console.log(`${num1} é maior que ${num2}`);
+}else{
+    console.log(`${num2} é maior que ${num1}`);
+}
