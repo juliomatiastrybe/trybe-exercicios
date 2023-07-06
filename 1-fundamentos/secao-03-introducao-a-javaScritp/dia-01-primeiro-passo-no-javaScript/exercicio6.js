cdda9d62d2c7bd59e4a0ec819300cef99d14d226
Julio Matias <juliocesark2@hotmail.com>
2023-07-06 01:47:19 -0300

    resolvendo o exercicio 6

---
+++ 1-fundamentos/secao-03-introducao-a-javaScritp/dia-01-primeiro-passo-no-javaScript/exercicio6.js
@@ -0,0 +1,12 @@
+let num1 = 3
+let num2 = 5
+let num3 = 8
+let numVerificacao;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0 ){
+    numVerificacao = true;
+    console.log(numVerificacao)
+} else{
+    numVerificacao = false;
+    console.log(numVerificacao);
+}
