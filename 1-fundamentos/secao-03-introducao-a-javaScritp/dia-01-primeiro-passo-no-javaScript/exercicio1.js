06fbc6d45bdb5c277e6a84127f5da26c1161eb53
Julio Matias <juliocesark2@hotmail.com>
2023-07-05 21:57:40 -0300

    Resolução do primeiro exercício

---
+++ 1-fundamentos/secao-03-introducao-a-javaScritp/dia-01-primeiro-passo-no-javaScript/exercicio1.js
@@ -0,0 +1,13 @@
+let num1 = 12
+let num2 = 6
+// Adição
+console.log(`A soma de ${num1} e ${num2} é = ${num1+num2}`)
+// subtração
+console.log(`A subtração de ${num1} e ${num2} é = ${num1-num2}`)
+// multiplicação
+console.log(`A multiplicação de ${num1} e ${num2} é = ${num1*num2}`)
+// divisão
+console.log(`A divisão de ${num1} e ${num2} é = ${num1/num2}`)
+// módulo ou cuocinte, resto da divião
+console.log(`O resto da divisão entre ${num1} e ${num2} é = ${num1%num2}`)
+
