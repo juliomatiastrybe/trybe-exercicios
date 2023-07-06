1434d1f15b95846585bd6425920746134e8f4f92
Julio Matias <juliocesark2@hotmail.com>
2023-07-06 00:01:06 -0300

    add arquivo exercicio4 e sua resolução

---
+++ 1-fundamentos/secao-03-introducao-a-javaScritp/dia-01-primeiro-passo-no-javaScript/exercicio4.js
@@ -0,0 +1,11 @@
+//Utilize if...else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+let angle1 = 60
+let angle2 = 40
+let angle3 = 80
+let soma = angle1 + angle2 + angle3
+
+if (soma == 180){
+    console.log(soma == 180);
+}else{
+    console.log(soma == 180);
+}
