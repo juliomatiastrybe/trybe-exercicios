e80a351995c278790588b72c6b48ba8d13cdd650
Julio Matias <juliocesark2@hotmail.com>
2023-07-07 02:04:13 -0300

    resolvendo o exercicio de fatorial

---
+++ 1-fundamentos/secao-03-introducao-a-javaScritp/dia-02-array-e-estrutrura-de-repeticao/Desafio fatorial.js
@@ -0,0 +1,8 @@
+//crie um algoritmo que imprima na tela o fatorial de 10.
+let number = 10
+let factorial = 1
+
+for (let index = 1; index <= number; index += 1){
+    factorial *= index;
+}
+console.log(factorial);
