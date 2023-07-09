9f422cfd34f1b3f8b36f195e47d5583306bcb05c
Julio Matias <juliocesark2@hotmail.com>
2023-07-09 00:52:30 -0300

    fazendo algumas alterações em fun-sec3-dia2

--- 1-fundamentos/secao-03-introducao-a-javaScritp/dia-02-array-e-estrutrura-de-repeticao/Desafio de inverter palavra.js
+++ 1-fundamentos/secao-03-introducao-a-javaScritp/dia-02-array-e-estrutrura-de-repeticao/Desafio de inverter palavra.js
@@ -1,7 +1,7 @@
 //Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra, como a palavra “banana” para “ananab”. Utilize a string abaixo como exemplo. Depois, troque-a por outras para verificar se seu algoritmo está funcionando corretamente.
 let word = 'tryber';
 let reverseWord = ''
-//let lastLetter = ''
+// let lastLetter = ''
 // reverseWord = word.split('').reverse().join('');
 // console.log(reverseWord);
 // método com split, reverse e join.
@@ -9,7 +9,8 @@ let reverseWord = ''
 for (let index = word.length - 1; index >= 0; index -= 1) {
     reverseWord += word[index]
 }
-//lastLetter +=  word[word.length-1]; 
+// lastLetter = word[word.length-1]; 
 // uma maneira de pegar a ultima letra do array word.
 
 console.log(reverseWord);
+// console.log(lastLetter);
