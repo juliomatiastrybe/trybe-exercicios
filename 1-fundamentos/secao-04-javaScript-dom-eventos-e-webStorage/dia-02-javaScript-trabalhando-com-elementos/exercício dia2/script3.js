cfca227737601f6e943a0f2bcca355859bca6cd6
Julio Matias <juliocesark2@hotmail.com>
2023-07-16 16:08:26 -0300

    fazendo exercícios 3 do funda-sec4-dia2

--- 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-02-javaScript-trabalhando-com-elementos/exercício dia2/script3.js
+++ 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-02-javaScript-trabalhando-com-elementos/exercício dia2/script3.js
@@ -2,4 +2,14 @@
 
 const whereAreYou = document.getElementById('where-are-you');
 // const filhoWhereAreYou = whereAreYou.firstElementChild; // uma outra maneira para remover o filho de whareAreYou.
-whereAreYou.removeChild(whereAreYou.firstElementChild);
+// whereAreYou.removeChild(whereAreYou.firstElementChild);
+
+const parent = document.getElementById('parent');
+const allChildren = parent.childNodes;
+for (let index = allChildren.length - 1; index >= 0; index -= 1) {
+    const currentChild = allChildren[index];
+
+    if (currentChild.id === 'where-are-you') { // verifica se o id do filho atual é igual a 'where-are-you'
+      currentChild.removeChild(currentChild.firstElementChild); // remove o primeiro elemento do filho atual, no caso a tag <h2>
+    }
+  };
