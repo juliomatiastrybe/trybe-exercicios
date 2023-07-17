06f4834813b879eabc203fdfc63391c91bdc546c
Julio Matias <juliocesark2@hotmail.com>
2023-07-16 21:44:56 -0300

    fazendo exercícios aprofunde conhecimento-iten-2,3,4  do funda-sec4-dia2

--- 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-02-javaScript-trabalhando-com-elementos/Exercícios - aprofunde seu conhecimento/script.js
+++ 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-02-javaScript-trabalhando-com-elementos/Exercícios - aprofunde seu conhecimento/script.js
@@ -6,3 +6,23 @@ h1.innerHTML = 'TrybeTrip - Agência de Viagens'
 
 body.appendChild(h1);
 
+// 2 -Adicione a tag main com a classe main-content como filho da tag body
+
+const main = document.createElement('main');
+main.className = 'main-content';
+
+body.appendChild(main);
+
+// 3 - Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+
+const sectionMain = document.createElement('section');
+main.className = 'center-content';
+
+body.appendChild(sectionMain);
+
+// 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+const pSection = document.createElement('p');
+pSection.innerText = 'Você vai viajar com com a melhor companhia Aéria';
+
+sectionMain.appendChild(pSection);
