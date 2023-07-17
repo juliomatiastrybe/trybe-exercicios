c2edf4c80c0f137daf45421f77d561766dd939f9
Julio Matias <juliocesark2@hotmail.com>
2023-07-17 00:57:17 -0300

    fazendo exercícios aprofunde conhecimento-iten-5,6,7,8,9 e bonus  do funda-sec4-dia2

--- 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-02-javaScript-trabalhando-com-elementos/Exercícios - aprofunde seu conhecimento/script.js
+++ 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-02-javaScript-trabalhando-com-elementos/Exercícios - aprofunde seu conhecimento/script.js
@@ -15,14 +15,98 @@ body.appendChild(main);
 
 // 3 - Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 
-const sectionMain = document.createElement('section');
+const centerContent = document.createElement('section');
 main.className = 'center-content';
 
-body.appendChild(sectionMain);
+main.appendChild(centerContent);
 
 // 4 - Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 
 const pSection = document.createElement('p');
 pSection.innerText = 'Você vai viajar com com a melhor companhia Aéria';
 
-sectionMain.appendChild(pSection);
+centerContent.appendChild(pSection);
+
+// 5 -Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+const leftContent = document.createElement('section');
+leftContent.className = 'left-content';
+
+main.appendChild(leftContent);
+
+// 6 - Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+
+const rightContent = document.createElement('section');
+rightContent.className = 'right-content';
+
+main.appendChild(rightContent);
+
+// 7 - Adicione uma imagem com src configurado para valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
+
+const smallImage = document.createElement('img');
+smallImage.src = 'https://picsum.photos/200';
+smallImage.className = 'small-image';
+
+leftContent.appendChild(smallImage);
+
+// 8 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, e assim por diante. Essa lista deve ser filha do section criado no passo 6;
+const ulRigthContent = document.createElement('ul');
+rightContent.appendChild(ulRigthContent);
+ 
+let listNumberInFull = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez' ];
+
+for (let index = 0; index < listNumberInFull.length; index += 1) {
+    let listNumber = listNumberInFull[index];
+
+    let listItem = document.createElement('li');
+    listItem.innerHTML = listNumber;
+    ulRigthContent.appendChild(listItem);
+};
+
+//Adicione 3 tags h3, todas filhas do main criado no passo 2.
+const h3firt = document.createElement('h3');
+h3firt.innerText = 'Olá!'
+const h3Second = document.createElement('h3');
+h3Second.innerText = 'Olá!'
+const h3Third = document.createElement('h3');
+h3Third.innerText = 'Olá!'
+main.appendChild(h3firt);
+main.appendChild(h3Second);
+main.appendChild(h3Third);
+
+//Adicione a classe title na tag h1 criada;
+
+const title = document.querySelector('h1');
+title.className = 'title';
+
+// title.appendChild(h1);
+
+//Adicione a classe description nas 3 tags h3 criadas;
+
+const h3 = document.querySelectorAll('h3');
+for (let index = 0; index < h3.length; index += 1) {
+    let itemH3 = h3[index]
+    itemH3.className = 'description';
+};
+//Bônus
+//Remova a section criada no passo 5 (aquela que possui a classe left-content) por meio da função .removeChild();
+
+main.removeChild(leftContent);
+
+//Centralize a section criada no passo 6 (aquela que possui a classe right-content).
+
+rightContent.style.marginRight = 'auto';
+
+//Troque a cor de fundo do elemento-pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
+
+main.style.backgroundColor = 'green'
+
+//Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
+
+const dez = ulRigthContent.lastElementChild;
+ulRigthContent.removeChild(dez);
+//poderia ser também:ulRigthContent.lastElementChild.remove();
+
+const nove = ulRigthContent.lastElementChild;
+ulRigthContent.removeChild(nove);
+// poderia usar o 2x: ulRigthContent.lastElementChild.remove();
