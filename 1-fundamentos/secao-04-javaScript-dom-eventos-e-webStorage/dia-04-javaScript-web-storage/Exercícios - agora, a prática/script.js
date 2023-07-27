a84a2c78a4ec522ee739a3633493bb2158997129
Julio Matias <juliocesark2@hotmail.com>
2023-07-26 22:04:16 -0300

    Exercícios - agora, a prática de funda-sec4-dia4, exercício resolvido

--- 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-04-javaScript-web-storage/Exercícios - agora, a prática/script.js
+++ 1-fundamentos/secao-04-javaScript-dom-eventos-e-webStorage/dia-04-javaScript-web-storage/Exercícios - agora, a prática/script.js
@@ -1,14 +1,52 @@
 window.onload = () => {
-    // estaura o backgroundColor
+    // restaura o as preferencias escolhidas do localStorage
     const restoreBackgroundColor = () => {
       if (localStorage.getItem('backgroudColor') !== null) {
-        const restoreColor = JSON.parse(localStorage.getItem('backgroudColor'));
+        const restoreBackground = JSON.parse(localStorage.getItem('backgroudColor'));
         const constent = document.querySelector(".content");
-        constent.style.backgroundColor = restoreColor
+        constent.style.backgroundColor = restoreBackground;
       }
     }
     restoreBackgroundColor();
-    // guarda o backgroundColor
+
+    const restoreColor = () => {
+      if (localStorage.getItem('color') !== null) {
+        const restoreColor = JSON.parse(localStorage.getItem('color'));
+        const constent = document.querySelector(".content");
+        console.log(constent);
+        constent.style.color = restoreColor;
+      }
+    }
+    restoreColor();
+
+    const restoreFontSize = () => {
+      if (localStorage.getItem('FontSize') !== null) {
+        const restoreFontSize = JSON.parse(localStorage.getItem('FontSize'));
+        const constent = document.querySelector(".paragraph");
+        constent.style.fontSize = restoreFontSize;
+      }
+    }
+    restoreFontSize();
+
+    const restorelineHeight = () => {
+      if (localStorage.getItem('lineHeight') !== null) {
+        const restorelineHeight = JSON.parse(localStorage.getItem('lineHeight'));
+        const constent = document.querySelector(".paragraph");
+        constent.style.lineHeight = restorelineHeight;
+      }
+    }
+    restorelineHeight();
+
+    const restoreFontFamily = () => {
+      if (localStorage.getItem('FontFamily') !== null) {
+        const restoreFontFamily = JSON.parse(localStorage.getItem('FontFamily'));
+        const constent = document.querySelector(".paragraph");
+        constent.style.fontFamily = restoreFontFamily;
+      }
+    }
+    restoreFontFamily();
+
+    // guarda o as preferencia escolhidas no localStorage
     const storageBackgroudColor = (backgroudColor) => {
       if (localStorage.getItem('backgroudColor') === null) {
         localStorage.setItem('backgroudColor', JSON.stringify(backgroudColor));
@@ -17,33 +55,66 @@ window.onload = () => {
       }
     }
     
+    const storageColor = (color) => {
+      if (localStorage.getItem('color') === null) {
+        localStorage.setItem('color', JSON.stringify(color));
+      } else if (color !== Object.values(localStorage)) {
+        localStorage.setItem('color', JSON.stringify(color));
+      }
+    }
+
+    const storageFontSize = (size) => {
+      if (localStorage.getItem('FontSize') === null) {
+        localStorage.setItem('FontSize', JSON.stringify(size));
+      } else if (size !== Object.values(localStorage)) {
+        localStorage.setItem('FontSize', JSON.stringify(size));
+      }
+    }
+
+    const storagelineHeight = (height) => {
+      if (localStorage.getItem('lineHeight') === null) {
+        localStorage.setItem('lineHeight', JSON.stringify(height));
+      } else if (height !== Object.values(localStorage)) {
+        localStorage.setItem('lineHeight', JSON.stringify(height));
+      }
+    }
+
+    const storageFontFamily = (family) => {
+      if (localStorage.getItem('FontFamily') === null) {
+        localStorage.setItem('FontFamily', JSON.stringify(family));
+      } else if (family !== Object.values(localStorage)) {
+        localStorage.setItem('FontFamily', JSON.stringify(family));
+      }
+    }
+
+    // aplica a cor de fundo quando o evento do click do buton backgroundColor
     const setBackgroundColor = (color) => {
       let content = document.querySelector(".content")
       content.style.backgroundColor = color
     }
 
-
+    // aplica cor no texto quando evento do click do buton color fonte
     const setFontColor = (color) => {
       let paragraphs = document.querySelectorAll(".paragraph")
       for (let index = 0; index < paragraphs.length; index += 1) {
         paragraphs[index].style.color = color
       }
     }
-  
+    // aplica tamanho da fonte
     const setFontSize = (size) => {
       let paragraphs = document.querySelectorAll(".paragraph")
       for (let index = 0; index < paragraphs.length; index += 1) {
         paragraphs[index].style.fontSize = size
       }
     }
-  
+    // aplica espaçamento entre as linhas
     const setLineHeight = (height) => {
       let paragraphs = document.querySelectorAll(".paragraph")
       for (let index = 0; index < paragraphs.length; index += 1) {
         paragraphs[index].style.lineHeight = height
       }
     }
-  
+    // aplica tipo de fonte
     const setFontFamily = (family) => {
       let paragraphs = document.querySelectorAll(".paragraph")
       for (let index = 0; index < paragraphs.length; index += 1) {
@@ -65,6 +136,7 @@ window.onload = () => {
     for (let index = 0; index < fontColorButtons.length; index += 1) {
       fontColorButtons[index].addEventListener("click", (event) => {
         setFontColor(event.target.innerHTML)
+        storageColor(event.target.innerHTML)
       })
     }
   
@@ -73,6 +145,7 @@ window.onload = () => {
     for (let index = 0; index < fontSizeButtons.length; index += 1) {
       fontSizeButtons[index].addEventListener("click", (event) => {
         setFontSize(event.target.innerHTML)
+        storageFontSize(event.target.innerHTML)
       })
     }
   
@@ -81,6 +154,7 @@ window.onload = () => {
     for (let index = 0; index < lineHeightButtons.length; index += 1) {
       lineHeightButtons[index].addEventListener("click", (event) => {
         setLineHeight(event.target.innerHTML)
+        storagelineHeight(event.target.innerHTML)
       })
     }
   
@@ -89,6 +163,7 @@ window.onload = () => {
     for (let index = 0; index < fontFamilyButtons.length; index += 1) {
       fontFamilyButtons[index].addEventListener("click", (event) => {
         setFontFamily(event.target.innerHTML)
+        storageFontFamily(event.target.innerHTML)
       })
     }
   }
