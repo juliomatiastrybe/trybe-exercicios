06a696033bdd5ce5ba1797cfffebbaf5cf9e0868
Julio Matias <juliocesark2@hotmail.com>
2023-08-23 01:08:19 -0300

    exercicio 1 sort, ok! fund-sec07-dia-02

---
+++ 1-fundamentos/secao-07-Métodos de Array/Dia 02: Métodos de Array - filter e sort/sort/exercicio 1.js
@@ -0,0 +1,19 @@
+// A nova tarefa do seu time de desenvolvimento é organizar o sistema de pessoas colaboradoras de uma rede de mercados. Para isso:
+
+// Utilize o sort para ordenar o array pela idade das pessoas em ordem crescente.
+
+// Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+//   people.sort((a, b) => a.age - b.age) // orden crescente.
+
+  people.sort((a, b) => b.age - a.age); // decrescente. 
+  
+  console.log(people);
