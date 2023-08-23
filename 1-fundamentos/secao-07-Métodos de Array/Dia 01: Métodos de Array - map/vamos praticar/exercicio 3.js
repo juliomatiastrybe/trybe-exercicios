5ff0833db79f966d97b3038b608763a85d490872
Julio Matias <juliocesark2@hotmail.com>
2023-08-22 23:36:32 -0300

    exercicio 3 vamos praticar agora sim ok! fund-sec07-dia-01

--- 1-fundamentos/secao-07-Métodos de Array/Dia 01: Métodos de Array - map/vamos praticar/exercicio 3.js
+++ 1-fundamentos/secao-07-Métodos de Array/Dia 01: Métodos de Array - map/vamos praticar/exercicio 3.js
@@ -6,7 +6,9 @@ const booksList = require('./listadelivros');
 // formato: Nome do livro - Gênero - Nome da pessoa autora.
 
 const formatedBookNames = (books) => {
-    const arrayBooks = books.map((book) => [ book.name, book.genre, book.author.name ]);
+    const arrayBooks = books.map((book) => {
+        return `${book.name} - ${book.genre} - ${book.author.name}`;
+    })
     return arrayBooks;
 };
 
@@ -14,11 +16,11 @@ const formatedBookNames = (books) => {
 
 // resumindo o return do books.map
 // const formatedBookNames = (books) => {
-//     const arrayBooks = books.map((book) => [ book.name, book.genre, book.author.name ]);
+//     const arrayBooks = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
 //     return arrayBooks;
 // };
 
 // resumindo o return da função formatBookNames e o return do books.map;
-// const formatedBookNames = (books) => arrayBooks = books.map((book) => [ book.name, book.genre, book.author.name ]);
+// const formatedBookNames = (books) => arrayBooks = books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}` );
 
 console.log(formatedBookNames(booksList.books));
