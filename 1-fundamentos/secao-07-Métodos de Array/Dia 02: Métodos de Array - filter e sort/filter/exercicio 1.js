d025234479cea772ae029f149bd0f7e097e8fbc8
Julio Matias <juliocesark2@hotmail.com>
2023-08-23 00:52:43 -0300

    exercicio 1 e 2 filter, ok! fund-sec07-dia-02

---
+++ 1-fundamentos/secao-07-Métodos de Array/Dia 02: Métodos de Array - filter e sort/filter/exercicio 1.js
@@ -0,0 +1,17 @@
+// Exercício 1
+// O array de objetos people, contêm elementos que representam pessoas 
+// com as propriedades name e age. 
+// Utilize o método filter para filtrar as 
+// pessoas que têm a idade igual ou superior a 30.
+
+const peoples = [
+    { name: 'João', age: 25 },
+    { name: 'Maria', age: 30 },
+    { name: 'Pedro', age: 20 },
+    { name: 'Ana', age: 35 },
+  ];
+
+  const ageBigger30 = peoples.filter(({ age }) => age >= 30);
+
+  console.log(ageBigger30);
+
---
+++ 1-fundamentos/secao-07-Métodos de Array/Dia 02: Métodos de Array - filter e sort/filter/exercicio 2.js
@@ -0,0 +1,17 @@
+// Exercício 2
+// Considere o seguinte array de objetos que representa uma lista de jogadores de basquete:
+
+// Utilizando o método filter e array destructuring, 
+// crie uma função filterHighPlayers que retorna um novo array 
+// contendo apenas os jogadores com altura igual ou superior a 200 centímetros.
+const players = [
+  { name: 'Michael Jordan', height: 198, team: 'Chicago Bulls' },
+  { name: 'LeBron James', height: 203, team: 'Los Angeles Lakers' },
+  { name: 'Kobe Bryant', height: 198, team: 'Los Angeles Lakers' },
+  { name: 'Stephen Curry', height: 191, team: 'Golden State Warriors' },
+  { name: 'Kevin Durant', height: 211, team: 'Brooklyn Nets' },
+];
+
+const filterHighPlayers = (players) => players.filter(({ height }) => height > 200 );
+
+console.log(filterHighPlayers(players));
