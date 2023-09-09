2d2b88e59a82e03f2731158dde21a8a433b4c4ac
Julio Matias <juliocesark2@hotmail.com>
2023-09-08 21:30:17 -0300

    exercicio de fixação ok do exemplo de front-sec1-dia1

--- 2-front-end/secao-01-introducao-ao-frantend-e-javaScript-assincrono/dia-01-ambiente-de-desenvolvimento/Iniciando um projeto com NPM/src/main.js
+++ 2-front-end/secao-01-introducao-ao-frantend-e-javaScript-assincrono/dia-01-ambiente-de-desenvolvimento/Iniciando um projeto com NPM/src/main.js
@@ -1,10 +1,22 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
+const copyBtn = document.querySelector('#copy-password');
+const displayAlertEl = document.querySelector('p');
 
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+copyBtn.addEventListener('click', () => {
+  if (displayPasswordEl.innerHTML === '...') {
+    displayAlertEl.innerHTML = 'Senha precisa ser gerada...';
+  } else {
+    copy(displayPasswordEl.innerHTML);
+    displayAlertEl.innerHTML = 'Senha copiada.';
+  }
 });
